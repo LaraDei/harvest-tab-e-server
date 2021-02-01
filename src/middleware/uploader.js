04ad57741af692177dde39b5212ffeaa486fd748
Lara Samuelson <lara.samuelson@gmail.com>
2021-02-01 15:43:46 -0800

    fix uploader file filter

--- src/middleware/uploader.js
+++ src/middleware/uploader.js
@@ -9,7 +9,7 @@ const s3 = new aws.S3({
  region: 'us-west-1'
 });
 
-const checkFileType = (file, cb) => {
+const checkFileType = (req, file, cb) => {
   if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png'){
     cb(null, true)
   } else {
@@ -18,13 +18,11 @@ const checkFileType = (file, cb) => {
 };
 
 const upload = multer({
+fileFilter: checkFileType,
 storage: multerS3({
   s3: s3,
   bucket: 'harvesttable',
   limits: {fileSize: 5000000},
-  fileFilter: function( req, file, cb ){
-    checkFileType( file, cb );
-   },
   metadata: function (req, file, cb) {
     cb(null, {fieldName: file.fieldname});
   },
